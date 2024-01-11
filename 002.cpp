b510ef029a569d94f1cabc62aa065802a9137c90
LSA31 <tmddk811@naver.com>
2024-01-11 22:44:20 +0900

    1546

---
+++ 002.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	int arr[1000];
+//
+//	cin >> n;
+//	for (int i = 0; i < n; i++)
+//		cin >> arr[i];
+//
+//	double sum = 0;
+//	double Max = 0;
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		if (arr[i] > Max)
+//			Max = arr[i];
+//		sum += arr[i];
+//	}
+//	cout << sum * 100 / Max / n;
+//}
